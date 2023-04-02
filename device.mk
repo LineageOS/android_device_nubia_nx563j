c60eaefd5960a7dc3879b51accfcfda1a1d276ff
Cyborg2017 <wangyang0987654@gmail.com>
2023-04-02 22:26:54 +0800

    nx563j: Decommonize keymaster and gatekeeper
    Change-Id: I99ccee67f4050ccb4c80e50beb84c4503ac43d0e

--- device.mk
+++ device.mk
@@ -58,6 +58,16 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/nubia_synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/nubia_synaptics_dsx.kl
